f8304d4273d5a56c65e7a55e2891870698eb1f03
MistbornL <lashaloria3@gmail.com>
2022-04-04 14:14:04 +0400

    update to classes

--- main.py
+++ main.py
@@ -2,13 +2,48 @@ from bs4 import BeautifulSoup
 import requests
 
 
-band_inp = input("Enter band name: ")
-song_inp = input("Enter song name: ")
-band = band_inp.replace(" ", "-")
-song = song_inp.replace(" ", "-")
+class Parser:
+    def __init__(self, url, tag, name):
+        self.url = url
+        self.tag = tag
+        self.name = name
 
-url = f"https://genius.com/{band}-{song}-lyrics"
-r = requests.get(url, 'html.parser')
-soup = BeautifulSoup(r.content)
-result = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-6 jYfhrf").text
+    def parse_web(self):
+        r = requests.get(self.url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        result = soup.find(self.tag, class_=self.name).text
+        return result
 
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__} url: {self.url}, tag: {self.tag}, name:{self.name}"
+        )
+
+
+class Song(Parser):
+    def __init__(self, band, song, url, tag, name):
+        super().__init__(url, tag, name)
+        self.band = band
+        self.song = song
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__} band: {self.band}, song: {self.song}"
+        )
+
+
+def main():
+    band_inp = input("Enter band name: ")
+    song_inp = input("Enter song name: ")
+    band = band_inp.replace(" ", "-")
+    song = song_inp.replace(" ", "-")
+
+    url = f"https://genius.com/{band}-{song}-lyrics"
+
+    s = Song(band, song, url, "div", "Lyrics__Container-sc-1ynbvzw-6 jYfhrf")
+    print(s.__repr__())
+    print(f"Lyric: {s.parse_web()}")
+
+
+if __name__ == "__main__":
+    main()
--- services/parser.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-def parser(url, tag, name):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    result = soup.find(tag, class_=name).text
-    return result
